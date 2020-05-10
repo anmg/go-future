a5d735e2dc673fb4c4d855b9758eb4e8faec8e5a
mingguo <mingguo@staff.weibo.com>
2020-05-10 10:37:49 +0800

    树、slice

--- cmd/main.go
+++ cmd/main.go
@@ -1,32 +1,65 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"net/http"
+	"fmt"
+	//"github.com/gin-gonic/gin"
+	//"time"
 )
 
-/*
+// 返回2个函数类型的返回值
+func test01(base int) (func(int) int, func(int) int) {
+	// 定义2个函数，并返回
+	// 相加
+	add := func(i int) int {
+		base += i
+		return base
+	}
+	// 相减
+	sub := func(i int) int {
+		base -= i
+		return base
+	}
+	// 返回
+	return add, sub
+}
+
 func main() {
-	http.HandleFunc("/hello", hello_http.HelloServer)
-	http.HandleFunc("/time", hello_http.TimeServer)
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+	f1, f2 := test01(10)
+	// base一直是没有消
+	fmt.Println(f1(1), f2(2))
+	// 此时base是9
+	fmt.Println(f1(3), f2(4))
+}
+/*
+// 定义中间
+func MiddleWare() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		t := time.Now()
+		fmt.Println("中间件开始执行了")
+		// 设置变量到Context的key中，可以通过Get()取
+		c.Set("request", "中间件")
+		status := c.Writer.Status()
+		fmt.Println("中间件执行完毕", status)
+		t2 := time.Since(t)
+		fmt.Println("time:", t2)
 	}
 }
-*/
-
-//RESTful路由get函数、
-func helloworldGet(c *gin.Context){
-	c.String(http.StatusOK, "Hello world in Get")
-}
 
 func main() {
-	router := gin.Default()
-
-	//RESTful路由
-	router.GET("/helloworld", helloworldGet)
-
-	//服务启动
-	router.Run("127.0.0.1:8082")
-}
+	// 1.创建路由
+	// 默认使用了2个中间件Logger(), Recovery()
+	r := gin.Default()
+	// 注册中间件
+	r.Use(MiddleWare())
+	// {}为了代码规范
+	{
+		r.GET("/ce", func(c *gin.Context) {
+			// 取值
+			req, _ := c.Get("request")
+			fmt.Println("request:", req)
+			// 页面接收
+			c.JSON(200, gin.H{"request": req})
+		})
+	}
+	r.Run()
+}*/
